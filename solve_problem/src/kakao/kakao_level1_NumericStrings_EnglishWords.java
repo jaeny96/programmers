9efa5e376a9d7868153f160d3edfdbfcdc7219bd
jaeny96 <42085171+jaeny96@users.noreply.github.com>
2021-08-09 17:48:47 +0900

    .

--- solve_problem/src/kakao/kakao_level1_NumericStrings_EnglishWords.java
+++ solve_problem/src/kakao/kakao_level1_NumericStrings_EnglishWords.java
@@ -3,8 +3,17 @@ package kakao;
 public class kakao_level1_NumericStrings_EnglishWords {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String str = "one4seveneight";
+		System.out.println(solution(str));
 	}
 
+	public static int solution(String s) {
+		String[] numbers = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+		String[] words = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+		for (int i = 0; i < numbers.length; i++) {
+			s = s.replaceAll(words[i], numbers[i]);
+		}
+		return Integer.parseInt(s);
+	}
 }
