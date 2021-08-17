e6f21eb7721aab946487e20083f8cfe917d276cc
jaeny96 <42085171+jaeny96@users.noreply.github.com>
2021-08-18 01:19:16 +0900

    .

---
+++ solve_problem/src/level1/practice/practice_sortingStringMyOwnWay.java
@@ -0,0 +1,15 @@
+//문자열 내 마음대로 정렬하기
+package level1.practice;
+
+public class practice_sortingStringMyOwnWay {
+
+	public static void main(String[] args) {
+		String[] strings = {"sun","bed","car"};
+		int n=1;
+
+	}
+    public static String[] solution(String[] strings, int n) {
+        String[] answer = {};
+        return answer;
+    }
+}
--- solve_problem/src/level1/practice/practice_sumBetweenTwoIntegers.java
+++ solve_problem/src/level1/practice/practice_sumBetweenTwoIntegers.java
@@ -4,7 +4,19 @@ package level1.practice;
 public class practice_sumBetweenTwoIntegers {
 
 	public static void main(String[] args) {
-		
+		int a=3;
+		int b=5;
+		System.out.println(solution(a,b));
 	}
-
+    public static long solution(int a, int b) {
+        long answer = 0;
+        if(a!=b) {
+        	for(int i=Integer.min(a, b); i<=Integer.max(a, b); i++) {
+        		answer+=i;
+        	}        	
+        }else {
+        	answer=a;
+        }
+        return answer;
+    }
 }
