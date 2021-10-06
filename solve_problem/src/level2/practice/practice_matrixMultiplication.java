3acb45851f9e121d3bd24eb6e39cfbbf6620aff8
jaeny96 <42085171+jaeny96@users.noreply.github.com>
2021-10-06 10:30:58 +0900

    10/6

---
+++ solve_problem/src/level2/practice/practice_matrixMultiplication.java
@@ -0,0 +1,28 @@
+package level2.practice;
+
+public class practice_matrixMultiplication {
+
+	public static void main(String[] args) {
+		int[][] arr1 = { { 1, 4 }, { 3, 2 }, { 4, 1 } };
+		int[][] arr2 = { { 3, 3 }, { 3, 3 } };
+		for (int[] arr : solution(arr1, arr2)) {
+			for (int a : arr) {
+				System.out.print(a + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static int[][] solution(int[][] arr1, int[][] arr2) {
+		int[][] answer = new int[arr1.length][arr2[0].length];
+
+		for (int i = 0; i < arr1.length; i++) {
+			for (int j = 0; j < arr2[0].length; j++) {
+				for (int k = 0; k < arr1[i].length; k++) {
+					answer[i][j] += arr1[i][k] * arr2[k][j];
+				}
+			}
+		}
+		return answer;
+	}
+}
